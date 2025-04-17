29587edd3b15355848618daa3aea5ff6b1b2f54d
LucynBernard <lucie.n.bernard@hotmail.com>
2025-04-17 17:49:07 +0200

    filtres html et css + Api

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -16,6 +16,24 @@ async function getWorks() {
     }
 }
 
+async function getCategory() {
+    const url = "http://localhost:5678/api/categories";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        for (let i = 0; i < json.length; i++) {
+            setFilter(json[i]);
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
 getWorks();
 
 function setFigure(data) {
@@ -26,3 +44,26 @@ function setFigure(data) {
     document.querySelector(".gallery").append(figure);
 }
 
+getCategory();
+
+
+
+function setFilter(data) {
+    const div = document.createElement("div")
+    div.innerHTML = `${data.name}`
+
+    document.querySelector(".gallery").append(div);
+}
+
+
+
+// // gestion des filtres
+// const boutonTous = document.querySelector(".btn-all")
+
+// const boutonObjets = document.querySelector(".btn-objets")
+
+// boutonObjets.addEventListener("click", function () {
+//     const objetsOnly = figure.filter(function (figure) {
+//         return figure.categoryId(1)
+//     })
+// })
